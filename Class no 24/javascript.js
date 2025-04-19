e9186569498ccd6cbeb125f47908e921c917e2c3
WajeehaArif22536-108437 <wajeehaarif172004@gmail.com>
2025-04-19 23:21:58 +0500

    first commit

---
+++ Class no 24/javascript.js
@@ -0,0 +1,23 @@
+x = 4;
+x=x+20;
+document.getElementById('demo1').innerHTML=x;
+x+=20;
+console.log(x)
+x-=20;
+console.log(x)
+x/=20;
+console.log(x)
+x%=20;
+console.log(x)
+first_value = 200
+second_value = 100
+console.log(first_value > second_value)
+console.log(first_value < second_value)
+console.log(first_value >= second_value)
+console.log(first_value <= second_value)
+console.log(first_value == second_value)
+console.log(first_value != second_value)
+console.log(first_value === second_value)
+console.log(first_value > second_value && first_value >= second_value)
+console.log(first_value > second_value || first_value  >= second_value)
+console.log (!(first_value < second_value))
